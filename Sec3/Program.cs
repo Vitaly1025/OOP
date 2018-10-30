5419b9d6896c67a591292931117c751eb8a5d16c
Vitaliy1025 <i.kurzenkov99@gmail.com>
2018-10-30 17:02:09 +0300

    C# Console

---
+++ Sec3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sec3
+{
+    class Program
+    {
+        public const double acc = 10;
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите длину сообщения");
+
+            int amount = Convert.ToInt32(Console.ReadLine());
+            double p;
+            double q;
+            double He=0;
+            double Hh = 0;
+
+            for (int i = 1; i <= amount; i++)
+            {
+                p = Chance(i);
+                q = 1 - p;
+                He = (-1) * (p * Math.Log(p, 2) + (q * Math.Log(q, 2)));
+                Hh = (-1) * (q * Math.Log(q, 2));
+                Console.WriteLine("p=" + p + "\n Потерю информации на каждый символ переданного сообщения:" + He +"бит"+ "\nЭффективная энтропия алфавита = " + (1-He)+" бит\n");
+
+            }
+            Console.WriteLine(He);
+        }
+
+      public static double Chance(int amount)
+        {
+
+            return (amount / acc);
+        }
+    }
+}
